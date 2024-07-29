fa4cdd5be0d8fa3846f5fde55d90401bb0d9e436
blanq31 <obiwulugodswill@gmail.com>
2024-07-30 00:24:56 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Adjust this if deploying to a subdirectory
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
